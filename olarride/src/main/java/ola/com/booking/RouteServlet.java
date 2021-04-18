6c20ad7721e6453d790cfdef59268997be58327a
Shivam Rai <Shivamrai007@users.noreply.github.com>
2021-04-18 20:04:20 +0530

    ola-ride
    Assignment

---
+++ olarride/src/main/java/ola/com/booking/RouteServlet.java
@@ -0,0 +1,59 @@
+package ola.com.booking;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ola.com.booking.model.Ride;
+import ola.com.booking.service.RideService;
+import ola.com.booking.service.RouteService;
+import ola.com.booking.service.impl.RideServiceImpl;
+import ola.com.booking.service.impl.RouteServiceImpl;
+
+@WebServlet("/routes")
+public class RouteServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	
+	private static final long serialVersionUID = 1L;
+	RouteService routeService = new RouteServiceImpl();
+	
+	RideService rideService = new RideServiceImpl();
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//u have userId
+	
+	        if(req.getParameter("action").equals("saveRides")) {
+				Ride ride = new Ride();
+				ride.setRideId(0);
+				ride.setRouteId(req.getParameter("routeId"));
+				ride.setUserId(req.getParameter("userId"));
+				ride.setDateTime("12/11/2021");
+				
+				rideService.saveRides(ride);	
+				RequestDispatcher rd1 = req.getRequestDispatcher("BookRideServlet");
+		        rd1.forward(req, resp);
+			
+			}else {
+
+				req.setAttribute("userId",req.getAttribute("userId")); //all rout info + userId
+				req.setAttribute("rList", routeService.getRoutes()); //all rout info + userId
+				RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/views/routeview.jsp");
+		        rd.forward(req, resp);
+		
+			}
+	
+		
+		
+	}
+
+	
+}
